29e6521c47cb2611f3995df1e8f547c96b58063c
lluisb3 <lluisb3@gmail.com>
2023-02-21 14:34:31 +0000

    minor updates

--- database/metadata_slides.py
+++ database/metadata_slides.py
@@ -12,7 +12,7 @@ thispath = Path(__file__).resolve()
 
 def metadata_csv():
     datadir = Path("/mnt/nas4/datasets/ToReadme/ExaMode_Dataset1/AOEC")
-    maskdir = Path(thispath.parent.parent / "data" / "Mask_PyHIST")
+    maskdir = Path(thispath.parent.parent / "data" / "Mask_PyHIST_v2")
 
     svs_files = natsorted([i for i in datadir.rglob("*.svs") if "LungAOEC" in str(i)], key=str)
 
@@ -25,19 +25,21 @@ def metadata_csv():
             if file.stem in name:
                 he_svs_files.append(file)
 
-    # for mask, file in natsorted(zip(binary_masks, he_svs_files), key=str):
-    #      print(file)
-    #      print(mask)
-
     print(f"Number of svs files for the metadata: {len(he_svs_files)}")
 
-    header = ["level_dimensions", "level_downsamples", "magnifications", "mpp", "number_patches_PyHist"]
+    header = ["level_dimensions", "level_downsamples", "magnifications", "mpp",
+              "number_patches_pyhist", "patch_shape", "center"]
 
     metadata = pd.DataFrame(columns=header)
 
     for svs_file in tqdm(he_svs_files, desc="Metadata .csv file in progress"):
-        
-        number_patches = len(os.listdir(Path(maskdir / svs_file.parent.stem / svs_file.stem / f"{svs_file.stem}_tiles")))
+        patchdir = Path(maskdir / svs_file.parent.stem / svs_file.stem / f"{svs_file.stem}_tiles")
+        number_patches = len(os.listdir(patchdir))
+        patch = cv.imread(str(Path(patchdir / os.listdir(patchdir)[0] )))
+        patch_shape = patch.shape
+        print(patch_shape)
+        center = svs_file.parent.stem.split("_")[0]
+        print(center)
 
         slide = openslide.OpenSlide(str(svs_file))
 
@@ -45,7 +47,8 @@ def metadata_csv():
         mpp = slide.properties['openslide.mpp-x']
         level_downsamples = slide.level_downsamples
         mags = available_magnifications(mpp, level_downsamples)
-        metadata.loc[svs_file.stem] = [level_dimensions, level_downsamples, mags, mpp, number_patches]
+        metadata.loc[svs_file.stem] = [level_dimensions, level_downsamples, mags,
+                                       mpp, number_patches, patch_shape, center]
 
     metadata.to_csv(f"{maskdir.parent}/metadata_slides.csv")
     print(f"metadata_slides.csv created in {maskdir.parent}")
