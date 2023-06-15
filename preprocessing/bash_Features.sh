c99de2436db0c39e5013fc8fcfb5e0f135703f9a
lluisb3 <lluisb3@gmail.com>
2023-06-15 09:16:16 +0000

    modify config to best

--- preprocessing/bash_Features.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-
-python3 -m preprocessing.store_features --config_file config_Features --exp_name_moco MoCo_resnet34_v2
-python3 -m preprocessing.store_features --config_file config_Features2 --exp_name_moco MoCo_resnet34_v2
